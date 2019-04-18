f5cabc55639c221e653ccb155bd5357360adf853
Udi h Bauman <dibaunaumh@gmail.com>
2019-04-18 07:01:52 -0400

    WIP auth changes
    Signed-off-by: Udi h Bauman <dibaunaumh@gmail.com>

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 
 const config = {
-	apiKey: "AIzaSyCtz7bxcOZ_9C3mR756qBJrCvlYw1zcRJY",
-	authDomain: "manual-pilot.firebaseapp.com",
-	databaseURL: "https://manual-pilot.firebaseio.com",
-	projectId: "manual-pilot",
-	storageBucket: "manual-pilot.appspot.com",
-	messagingSenderId: "599409639649"
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
 
